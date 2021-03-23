6a879097e1357cc64da31fa67766cbd0106af6ed
lol123a10-debug <ayubkhon.gulomov@gmail.com>
2021-03-23 21:51:31 +0500

    a

--- 15/app.js
+++ 15/app.js
@@ -13,4 +13,20 @@ console.log(purchaseTax(300));
 console.log(saleTax(300));
 console.log(saleTax(500));
 
-console.log(300);
+// function powerOf(x, n) {
+//   let result = 1;
+//   for (i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+
+function powerOf(x, n) {
+  // if (n === 1) {
+  //   return x;
+  // }
+  // return x * powerOf(x, n - 1);
+  return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 6)); // 1*2*2*2*2*2*2
